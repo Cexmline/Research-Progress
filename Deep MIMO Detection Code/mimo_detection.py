c2cf889f13f93197f0766382d8514ee477a35fa4
Cexmline <1242027077@qq.com>
2017-08-30 20:33:24 +0800

    H W都有待修正,明天争取run起来丫

--- Deep MIMO Detection Code/mimo_detection.py
+++ Deep MIMO Detection Code/mimo_detection.py
@@ -1,12 +1,27 @@
 import tensorflow as tf
+import numpy as np
 K=2   #Number of users
 N=4   #Number of receiving antenna
+#Define the batch size and channel matrix H_
+batch_size=500
+STEPS=500  #Number of iteration
+
+#H_=???????
 
 #Define the model of the system
+dataset_size=batch_size*K*STEPS
+source=np.random.randint(0,2,(dataset_size,1))
+x=-2*source+1
+x=np.reshape(x,(K,batch_size*STEPS))
 w=tf.random_normal([N,1],mean=0,stddev=1) #Noise Vector with independent,zero mean Gaussian variables of variance 1
-x=tf.placeholder(tf.float32,shape=(K,1),name="transmit")
-H=tf.placeholder(tf.float32,shape=(N,K),name="ChannelMatrix")
-y=tf.matmul(H,x)+w
+y_=tf.matmul(H,x)+w
+
+
+
+x=tf.placeholder(tf.float32,shape=(K,batch_size),name="transmit")
+y=tf.placeholder(tf.float32,shape=(N,batch_size),name="receiver")
+#H=tf.placeholder(tf.float32,shape=(N,K),name="ChannelMatrix")
+
 
 #Define the parameters of the network
 w1k=tf.Variable(tf.random_normal([8*K,5*K],stddev=1,seed=1))
@@ -49,6 +64,17 @@ train_step=tf.train.AdamOptimizer(0.001).minimize(lossfunction)
 sess=tf.Session()
 init_op=tf.global_variables_initializer()
 sess.run(init_op)
+
+for i in range(STEPS):
+	start=(i*batch_size)%dataset_size
+	end =min(start+batch_size,dataset_size)
+
+	sess.run(train_step,
+			feed_dict={x:X[start:end],y:y_[]})
+	if i%100==0:
+		total_lossfunction=sess.run(
+			lossfunction,feed_dict{x:X,y:y_})
+		print("After %d training steps,cross entropy on all data is %g"%(i,total_lossfunction))
 
 
 
